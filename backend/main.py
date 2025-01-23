9b43b1fa8a233357a44d439d3289a27617306540
David <david2020martin@gmail.com>
2025-01-23 11:49:35 -0500

    stability API call implementation

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,request,json
+from flask import Flask,request,json,jsonify
 import base64
 from flask_cors import CORS
 import logging
@@ -12,29 +12,65 @@ load_dotenv()
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-client  =OpenAI()
+client  = OpenAI()
 CORS(app)
+
+stable_diffusion_api_url = 'https://api.stability.ai/v2beta/stable-image/generate/ultra'
+stable_diffusion_apiKey = os.getenv("STABILITY_API_KEY")
 @app.route('/generate-prompt', methods =['POST'])
 def generate_prompt():
-
     try:
         response  = client.chat.completions.create(
-            model= "gpt-4",
+            model= "gpt-4o-mini",
             messages=[
-                {"role":"system","content":"You are a helpful assitant"},
-                {"role":"user","content":"What is the meaning of Life?"}
-            ]
+                {"role":"system","content":"You are given a scenerio and your job is to explain the scene in as much detail as possible"},
+                {"role":"user","content":"Bigfoot in a Mossy Green Forest"}
+            ],
         )
         api_response  =  response.choices[0].message.content
 
         print(f"AI Response: {api_response}")
 
-        return json.jsonify({"response": api_response})
-        
+        # Generate image using stability AI
+
+        # headers = {
+        #     "authorization": f"Bearer {stable_diffusion_apiKey}",
+        #     "accept": "image/*"
+        # },
+        # data = {
+        #     "prompt": [{"text": api_response}],
+        #     "cfg_scale": 7, 
+        #     "output_format": "webp",
+        #     "width": 1024,
+        #     "height": 1024,
+        #     "samples": 1,
+        # }
+        stability_response = requests.post(
+            stable_diffusion_api_url,
+            headers={
+                "authorization": f"Bearer {stable_diffusion_apiKey}",
+                "accept": "image/*"
+            },
+            files={"none": ' '
+                   
+            },
+            data={
+                "prompt": api_response,
+                "output_format": "jpeg"
+            },
+        )
+        if stability_response.status_code !=200:
+            logging.error(f"Error from Stability AI: {stability_response.text}")
+            return jsonify({"error": "Failed to generate image"}), 500
+        else:
+            with open("generated_image.jpeg","wb") as f:
+                f.write(stability_response.content)
+
+            return jsonify({"message": "Image successfully generated and saved as 'generated_image.png'"}), 20
     except Exception as e:
         logging.error(f"Error: {e}")
+      #  logging.error(f"Error from Stability AI: {stability_response.text}")
         return json.jsonify({"error": str(e)}), 500
-
-   
+    
 if __name__ == '__main__':
     app.run(debug=True)
