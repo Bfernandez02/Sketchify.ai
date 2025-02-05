eaad433937293395cc1d22cf7187f11a2bc10c4a
Erik Hansen <erhansen@civiconnect.ca>
2025-02-05 10:11:30 -0500

    desktop nav

--- frontend/webapp/src/components/Navbar.jsx
+++ frontend/webapp/src/components/Navbar.jsx
@@ -6,11 +6,17 @@ export default function Navbar() {
 		<>
 			<div className="flex justify-between items-center px-10 py-4">
 				<div className="font-italianno text-5xl">Sketchify</div>
-				<nav className="flex space-x-8 font-fraunces font-normal text-2xl uppercase">
+				<nav className="flex items-center space-x-8 font-fraunces font-normal text-2xl uppercase">
 					<Link href="/"> Home </Link>
 					<Link href="/sketch"> Sketch </Link>
 					<Link href="/explore"> Explore </Link>
 					<Link href="/contact"> Contact </Link>
+					<Link
+						href="/sign-in"
+						className="bg-blue text-white px-4 py-2  rounded-[20px]"
+					>
+						Login
+					</Link>
 				</nav>
 			</div>
 		</>
