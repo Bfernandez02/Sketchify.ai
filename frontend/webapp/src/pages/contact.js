7f5552174dcd8fe368bb14d35eed124e746ae202
Erik Hansen <erhansen@civiconnect.ca>
2025-02-05 15:01:02 -0500

    seo and page templates

---
+++ frontend/webapp/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NextSeo } from "next-seo";
+
+export default function contact() {
+	return (
+		<>
+			<NextSeo title="Contact | Sketchify" />
+			<div className="content-container">
+				<h1>Contact</h1>
+			</div>
+		</>
+	);
+}
--- frontend/webapp/src/pages/index.js
+++ frontend/webapp/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import LandingHero from "../components/LandingHero";
+import { NextSeo } from "next-seo";
 
 export default function index() {
 	return (
 		<>
+			<NextSeo title="Home | Sketchify" />
 			<div className="content-container">
 				<LandingHero />
 			</div>
