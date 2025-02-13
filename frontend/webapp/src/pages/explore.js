0e1f60a8a252dee0c322f4f1c25dc11c1d808333
Erik Hansen <erhansen@civiconnect.ca>
2025-02-13 14:53:59 -0500

    Hero

--- frontend/webapp/src/pages/explore.js
+++ frontend/webapp/src/pages/explore.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { NextSeo } from "next-seo";
+import ExploreHero from "@/components/ExploreHero";
 
 export default function explore() {
 	return (
 		<>
 			<NextSeo title="Explore | Sketchify" />
 			<div className="content-container">
-				<h1>Explore</h1>
+				<ExploreHero />
 			</div>
 		</>
 	);
