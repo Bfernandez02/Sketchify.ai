626c5dbdb77486d7daac880bb00485390850bffd
Erik Hansen <erhansen@civiconnect.ca>
2025-02-13 15:58:22 -0500

    responsive

--- frontend/webapp/src/components/Trending.jsx
+++ frontend/webapp/src/components/Trending.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import abs from "../../public/abs.jpg";
 import min from "../../public/min.jpg";
 import rea from "../../public/rea.jpg";
@@ -11,14 +11,20 @@ export default function Trending() {
 		{ name: "Realism", image: rea },
 	];
 
+	const [selectedTheme, setSelectedTheme] = useState("");
+	const reset = () => setSelectedTheme("");
+
 	return (
 		<div className="max-w-[1280px] mx-auto p-4">
-			<h2 className="font-fraunces text-4xl pb-4">Trending Themes</h2>
-			<div className="flex flex-row gap-12">
+			<h2 className="font-fraunces text-4xl pb-4 md:text-left text-center">
+				Trending Themes
+			</h2>
+			<div className="flex md:flex-row flex-col xl:gap-12 lg:gap-6 gap-4 items-center">
 				{trendingThemes.map((theme, index) => (
 					<div
 						key={index}
-						className="w-[400px] h-[200px] rounded-[20px] relative overflow-hidden"
+						className="md:w-[400px] md:h-[200px] w-full h-[120px] rounded-[20px] relative overflow-hidden hover:scale-[98%] transition-transform duration-300 hover:cursor-pointer"
+						onClick={() => setSelectedTheme(theme.name)}
 					>
 						{/* Image */}
 						<Image
@@ -38,6 +44,20 @@ export default function Trending() {
 						</h3>
 					</div>
 				))}
+			</div>
+			<div className="flex justify-between items-center pt-4">
+				<div>
+					Showing:{" "}
+					<span className="font-bold">
+						{selectedTheme ? selectedTheme : "All"}{" "}
+					</span>
+				</div>
+				<button
+					className="bg-secondary text-white font-roboto text-lg rounded-[20px]"
+					onClick={reset}
+				>
+					See All
+				</button>
 			</div>
 		</div>
 	);
