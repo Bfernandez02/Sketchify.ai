7e38a9744ea3fe11b8f8e85460196b979dc5641a
Brandon Fernandez <athemstorm.08@gmail.com>
2025-02-15 16:44:53 -0500

    Artwork Cards + Masonry (Explore) Fixes #59

--- frontend/webapp/src/pages/explore.js
+++ frontend/webapp/src/pages/explore.js
@@ -2,15 +2,21 @@ import React from "react";
 import { NextSeo } from "next-seo";
 import ExploreHero from "@/components/ExploreHero";
 import Trending from "@/components/Trending";
+import DisplayArtsGrid from "@/components/DisplayArtsGrid";
+import artsData from "@/utils/artsData";
 
 export default function explore() {
-	return (
-		<>
-			<NextSeo title="Explore | Sketchify" />
-			<div className="content-container">
-				<ExploreHero />
-				<Trending />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <NextSeo title="Explore | Sketchify" />
+      <div className="content-container">
+        <ExploreHero />
+        <Trending />
+
+        <div className="max-w-[1280px] mx-auto p-4">
+          <DisplayArtsGrid arts={artsData} simple = {false} />
+        </div>
+      </div>
+    </>
+  );
 }
