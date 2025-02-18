e70ade9eaa05e385cb7b5340460ac8592a2a779b
Erik Hansen <erhansen@civiconnect.ca>
2025-02-18 09:44:50 -0500

    art object structure

--- frontend/webapp/src/pages/art/[id].js
+++ frontend/webapp/src/pages/art/[id].js
@@ -1,9 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import pic from "../../../public/erik.png";
+import wand from "../../../public/wand.svg";
+import sketch from "../../../public/drawing.png";
+import enhanced from "../../../public/drawingEnh.png";
 
 export default function art() {
+	// example of art object we will use from firebase**
+	const art = {
+		id: 1,
+		title: "Swing hanging from tree on a beach, during a sunset",
+		categories: [{ id: 1, name: "Surrealism" }],
+		user: [{ username: "EHansen100" }, { profilePic: pic }],
+		date: "2023-08-15",
+		original: sketch,
+		enhanced: enhanced,
+	};
+
 	return (
-		<div>
-			<div>Artwork 1</div>
+		<div className="content-container">
+			<div className="flex justify-between">
+				<h2>{art.title}</h2>
+			</div>
 		</div>
 	);
 }
