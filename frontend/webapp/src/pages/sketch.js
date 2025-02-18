2ecac377d5b9bc35903781c80d8a16f74e07835d
David Martin <david2020martin@gmail.com>
2025-02-17 20:53:37 -0500

    sketch pad component fix

--- frontend/webapp/src/pages/sketch.js
+++ frontend/webapp/src/pages/sketch.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { NextSeo } from "next-seo";
 import SketchPad from "@/components/SketchPad";
+import pencil from "../../public/pencil.png";
 
 export default function sketch() {
-	return (
-		<>
-			<NextSeo title="Sketch | Sketchify" />
-			<div className="content-container ">
-				<h1 className="font-fraunces"> Sketch, Enhance, and Create, Bring Your Ideas to Life</h1>
-				<SketchPad/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <NextSeo title="Sketch | Sketchify" />
+      <div className="content-container ">
+        <div className="flex flex-row space-x-10 w-full items-center justify-center">
+          <div className=" w-1/2">
+            <h1 className="font-fraunces justify-center">
+              {" "}
+              Sketch, Enhance, and Create, Bring Your Ideas to Life
+            </h1>
+          </div>
+          <img className="w-50 h-40" src={pencil.src} alt="Pencil" />
+        </div>
+        <SketchPad />
+      </div>
+    </>
+  );
 }
