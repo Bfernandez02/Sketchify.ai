af6d2d62b48387d728bd93bb07d1b8e47378703e
Erik Hansen <erhansen@civiconnect.ca>
2025-02-19 22:53:42 -0500

    contact page added

---
+++ frontend/webapp/src/components/Contact.jsx
@@ -0,0 +1,164 @@
+import React from "react";
+import Image from "next/image";
+import stroke from "../../public/stroke.png";
+import brushes from "../../public/brushes.png";
+import abstract from "../../public/abstract.jpg";
+import astro from "../../public/astro.jpg";
+import bunny from "../../public/bunny.jpg";
+import cake from "../../public/cake.jpg";
+import forest from "../../public/forest.jpg";
+import Contact_Person from "../../public/Contact_Person.png";
+import Phone from "../../public/Phone.png";
+import Mail from "../../public/Mail.png";
+import Location from "../../public/Location.png";
+import Plane from "../../public/Plane.png";
+import Link from "next/link";
+
+export default function Contact() {
+	return (
+		<div>
+			<link
+				href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
+				rel="stylesheet"
+			/>
+			<link
+				href="https://fonts.googleapis.com/css2?family=Fraunces&display=swap"
+				rel="stylesheet"
+			/>
+			<link
+				href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap"
+				rel="stylesheet"
+			/>
+
+			<div className="w-full flex flex-wrap">
+				{/* Left Section */}
+				<div className="w-full md:w-2/3 p-5 flex items-center justify-start">
+					<h1 className="font-fraunces xl:text-8xl md:text-7xl text-5xl">
+						Have Questions? <br /> Get in Touch With Us!
+					</h1>
+				</div>
+
+				{/* Right Section */}
+				<div className="w-full md:w-1/3 text-center">
+					<Image
+						src={Contact_Person.src}
+						alt="Contact Image"
+						width={400}
+						height={520}
+						className="mx-auto"
+					/>
+				</div>
+			</div>
+
+			<div className="w-full flex flex-wrap bg-[#f8f6eb] p-5 rounded-lg mt-5 mb-[100px]">
+				{/* Left Section */}
+				<div className="w-full md:w-[35%] bg-[#2e3547] p-10 text-white rounded-tl-lg rounded-bl-lg mb-5">
+					<h2 className="font-playfair text-4xl mb-10">Contact Us</h2>
+
+					<p className="flex items-center mb-8 text-xl pl-10">
+						<Image
+							src={Phone}
+							alt="Phone"
+							width={20}
+							height={20}
+							className="object-contain mr-8"
+						/>
+						(123) 456-7890
+					</p>
+
+					<p className="flex items-center mb-8 text-xl pl-10">
+						<Image
+							src={Mail}
+							alt="Mail"
+							width={20}
+							height={20}
+							className="object-contain mr-8"
+						/>
+						<a
+							href="mailto:placeholder@email.com"
+							className="text-white"
+						>
+							placeholder@email.com
+						</a>
+					</p>
+
+					<p className="flex items-center mb-8 text-xl pl-10">
+						<Image
+							src={Location}
+							alt="Location"
+							width={20}
+							height={20}
+							className="object-contain mr-8"
+						/>
+						123 placeholder drive, city, province, postal code
+					</p>
+
+					<div className="flex flex-row pt-3 gap-x-6 items-center">
+						<Link
+							href={"/team"}
+							className="text-[18px] hover:underline"
+						>
+							OUR TEAM
+						</Link>
+						<i className="fa-brands fa-facebook text-secondary fa-2x"></i>
+						<i className="fa-brands fa-twitter text-primary text-xl bg-secondary rounded-full flex items-center justify-center w-[34px] h-[34px]"></i>
+						<i className="fa-brands fa-pinterest-p text-primary text-xl bg-secondary rounded-full flex items-center justify-center w-[34px] h-[34px]"></i>
+						<i className="fa-brands fa-instagram text-primary text-xl bg-secondary rounded-full flex items-center justify-center w-[34px] h-[34px]"></i>
+					</div>
+				</div>
+
+				{/* Right Section */}
+				<div className="w-full md:w-[65%] p-6 mt-5">
+					<form>
+						<div className="flex gap-5 mb-4 flex-wrap">
+							<input
+								type="text"
+								placeholder="Full Name"
+								className="flex-1 p-4 text-lg"
+							/>
+							<input
+								type="text"
+								placeholder="Phone Number"
+								className="flex-1 p-4 text-lg"
+							/>
+						</div>
+
+						<div className="flex gap-5 mt-4 mb-4 flex-wrap">
+							<input
+								type="email"
+								placeholder="Mail"
+								className="flex-1 p-4 text-lg"
+							/>
+							<input
+								type="text"
+								placeholder="Subject"
+								className="flex-1 p-4 text-lg"
+							/>
+						</div>
+
+						<textarea
+							placeholder="Message"
+							className="w-full h-[150px] mt-4 p-4 text-lg"
+						></textarea>
+
+						<div className="text-center mt-4">
+							<button
+								type="submit"
+								className="bg-[#d57c56] text-white py-5 px-24 mt-5 border-none cursor-pointer text-2xl font-playfair flex items-center"
+							>
+								Send Message
+								<img
+									src={Plane.src}
+									alt="Plane"
+									width={20}
+									height={20}
+									className="ml-4"
+								/>
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
