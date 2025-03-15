8f9669514e044ec22940c9e4b7aee4f6ea105b1c
Brandon Fernandez <athemstorm.08@gmail.com>
2025-03-14 20:49:54 -0400

    Auth, maintaining session + create user context Fixes #113

--- frontend/webapp/src/pages/_app.js
+++ frontend/webapp/src/pages/_app.js
@@ -3,14 +3,18 @@ import "@/styles/utility-classes.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "@/context/authContext";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Toaster />
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+
+      <AuthProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </AuthProvider>
     </>
   );
 }
