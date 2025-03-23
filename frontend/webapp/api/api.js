f27d2a60f62433af776aa92bb6b37389e3a532b2
Erik Hansen <erhansen@civiconnect.ca>
2025-03-23 14:45:07 -0400

    Revert "node module PR lol"
    This reverts commit 5e984c5e4ef9583ef73966488b507c488abf6014, reversing
    changes made to 264449458419e445fb6023e7925e2f626608eb4d.

--- frontend/webapp/api/api.js
+++ frontend/webapp/api/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 const  BASE_URL = "http://localhost:5001"
 
-export const CallApi = async(ImageData,ThemeData)=>{
+export const CallApi = async(ImageData)=>{
     try {
         const response = axios.post(`${BASE_URL}/generate-prompt`,{
             image: ImageData,
-            theme: ThemeData ||"Default"
         });
         return response;
     } catch (error) {
