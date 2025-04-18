4a1820418d7d20845de02940fef8195ce5826320
Hamza Waheed <psionictypo@gmail.com>
2025-04-18 16:20:30 -0400

    Google cloud run prod_server now integrated with webapp, and tested to work

--- frontend/webapp/api/api.js
+++ frontend/webapp/api/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
-const  BASE_URL = "http://localhost:5001"
 
-export const CallApi = async(ImageData,ThemeData,Prompt)=>{
+const LOCAL_URL = "http://localhost:5001";
+const PROD_URL = process.env.NEXT_PUBLIC_PROD_API_URL
+
+export const CallApi = async(ImageData, ThemeData, Prompt) => {
     try {
-        const response = axios.post(`${BASE_URL}/generate-prompt`,{
+        const response = await axios.post(`${PROD_URL}/generate-prompt`, {
             image: ImageData,
-            theme: ThemeData ||"Default",
-            userPrompt:Prompt||""
-            
-
+            theme: ThemeData || "Default",
+            userPrompt: Prompt || ""
         });
         return response;
     } catch (error) {
         console.error("Error:", error);
         return { error: "Something went wrong while calling the API" };
     }
-}
+};
