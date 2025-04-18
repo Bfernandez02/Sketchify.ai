3a0a3ca3c15811b3defdd58b1493c0d5f5e780cb
Erik Hansen <erhansen@civiconnect.ca>
2025-04-18 08:59:59 -0400

    build fixes: opt. dependancies & extra def export

--- frontend/webapp/src/pages/api/hello.js
+++ frontend/webapp/src/pages/api/hello.js
@@ -1,16 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
 
-
-const BASE_URL = ' '
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
-export default function FetchPicture(){
-
-  try {
-
-  } catch (error) {
-    
-  }
+const BASE_URL = " ";
+export default function FetchPicture() {
+	try {
+	} catch (error) {}
 }
