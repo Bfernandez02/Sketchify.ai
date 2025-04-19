4f601698b2695b5190708ecab401f678d4a05ee3
David Martin <david2020martin@gmail.com>
2025-04-19 17:02:03 -0400

    api call fix

--- frontend/webapp/api/api.js
+++ frontend/webapp/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const  BASE_URL = "http://localhost:5001"
 
 export const CallApi = async(ImageData,ThemeData,Prompt,Complexity)=>{
-    console.log
+    
     try {
         const response = axios.post(`${BASE_URL}/generate-prompt`,{
             image: ImageData,
